fdb1b0e550a3d5db85bcc639f6f7f7a937aa68f9
Charles2005 <abecharles5@gmail.com>
2020-12-04 18:06:53 +0800

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,21 +1,14 @@
-import Compostion.*;
-import ExampleOfClasses.*;
-import Inheritance.*;
+import Polymorphism.Jaws;
 
 public class Practice {
 
     public static void main(String[] args){
-       Dimension dimension = new Dimension(20,20,5);
-       Case theCase = new Case("NXT","ASUS", "550 watts", dimension );
 
-       Monitor theMonitor = new Monitor("32 inch beast", "Acer", 32, new Resolution(2540, 1400, 1080));
-       Motherboard theMotherboard = new Motherboard("B550", "Asus", 4, 8, "v.5.5");
+        Jaws jaws = new Jaws();
 
-       Pc pc = new Pc(theCase, theMonitor, theMotherboard);
-       pc.powerUp();
+        System.out.println(jaws.plot());
 
 
     } // End of main method
-
 
 }
